93dd29607522731eb1c64a6bce7ff7572a2104bb
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-07-31 20:45:01 -0300

    mercado pago

--- src/main/java/pitslify/api/config/AppConfig.java
+++ src/main/java/pitslify/api/config/AppConfig.java
@@ -1,11 +1,13 @@
 package pitslify.api.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Setter
 @Getter
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
@@ -18,7 +20,6 @@ public class AppConfig implements WebMvcConfigurer {
 
     @Value("${test.access.token}")
     private final String TEST_ACCESS_TOKEN = "TEST-7951072446836198-072618-2f79178cf1345b06d3118bf10ba8e5a1-618365626";
-
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
