b04d1e6a22c8a7ce3e6dd13f6d23bf5f1c1ca5e1
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-08-02 23:49:36 -0300

    refactors

--- src/main/java/pitslify/api/config/AppConfig.java
+++ src/main/java/pitslify/api/config/AppConfig.java
@@ -15,12 +15,6 @@ public class AppConfig implements WebMvcConfigurer {
     @Value("${cors.originPatterns}")
     private String originPatterns;
 
-    @Value("${prod.access.token}")
-    private String PROD_ACCESS_TOKEN;
-
-    @Value("${test.access.token}")
-    private final String TEST_ACCESS_TOKEN = "TEST-7951072446836198-072618-2f79178cf1345b06d3118bf10ba8e5a1-618365626";
-
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         var allowedOrigins = originPatterns.split(",");
